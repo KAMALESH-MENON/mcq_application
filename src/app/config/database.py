a7fcbd669de6918fe422a2f3c2f5c5f532008602
Kamalesh <kamalcbe86@gmail.com>
2024-12-24 15:37:36 +0000

    Initial database connection

---
+++ src/app/config/database.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("connection_url")
+
+if not DATABASE_URL:
+    raise ValueError("The connection URL is not set from the .env file.")
+
+engine = create_engine(DATABASE_URL)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+
+def get_db():
+    """
+    Create a database session.
+    Yields:
+        Session: The database session.
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+if __name__ == "__main__":
+    try:
+        Base.metadata.create_all(bind=engine)
+        print("Testing connection with database.")
+    except Exception as e:
+        print("An error occurred:", e)
