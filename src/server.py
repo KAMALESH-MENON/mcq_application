4e89814608e90c562164a9485e0efe4a23b736f2
Kamalesh <kamalcbe86@gmail.com>
2024-12-30 09:45:43 +0000

    initial server

--- src/server.py
+++ src/server.py
@@ -1,3 +1,8 @@
 from fastapi import FastAPI
+from src.app.config.database import Base, engine
+from src.app.models import data_models as models
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+print("Tables created.")
