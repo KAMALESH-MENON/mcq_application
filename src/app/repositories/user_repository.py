73fca295824a9c4f7772d22c5f5f12a4501f247a
Kamalesh <kamalcbe86@gmail.com>
2024-12-30 09:50:55 +0000

    "Implement UserRepository with SQLAlchemy"
    Details:
    
    - Created UserRepository.
    - Added database interaction methods (get, get_all, add, update, and delete) using SQLAlchemy's Session.

--- src/app/repositories/user_repository.py
+++ src/app/repositories/user_repository.py
@@ -1,44 +1,29 @@
+from uuid import UUID
 from sqlalchemy.orm import Session
-from sqlalchemy.exc import IntegrityError
-from src.app.models.user import User, UserRole
+from src.app.models.data_models import User
 from src.app.repositories.base_repository import BaseRepository
 
-class UserRepository(BaseRepository):
-    """Repository for User model to handle database operations."""
-
+class UserRepository(BaseRepository[User]):
     def __init__(self, session: Session):
         self.session = session
-
-    def get_one(self, id):
-        pass
-
+        
+    def get(self, id: UUID):
+        return self.session.query(User).filter(User.user_id == id).first()
+    
     def get_all(self):
-        pass
+        return self.session.query(User).all()
 
-    def add(self, username: str, password: str, role: UserRole = UserRole.USER) -> User:
-        """Create a new user."""
-        try:
-            new_user = User(username=username, password=password, role=role)
-            self.session.add(new_user)
-            self.session.commit()
-            self.session.refresh(new_user)
-            return new_user
-        except IntegrityError as error:
-            self.session.rollback()
-            raise ValueError("Username already exists") from error
+    def add(self, user: User):
+        self.session.add(user)
+        self.session.commit()
 
-    def update(self, id):
-        pass
+    def update(self, id: UUID, **kwargs):
+        user = self.get(id)
+        for key, value in kwargs.items():
+            setattr(user, key, value)
+        self.session.commit()
 
-    def delete(self, id):
-        pass
-
-if __name__ == "__main__":
-    from src.app.config.database import get_db
-    session = next(get_db())
-    test = UserRepository(next(get_db()))
-    try:
-        test.add(username="test_user4", password="test_password", role=UserRole.USER)
-        print("User added.")
-    except ValueError as e:
-        print(f"Error: {e}")
+    def delete(self, id: UUID):
+        user = self.get(id)
+        self.session.delete(user)
+        self.session.commit()
