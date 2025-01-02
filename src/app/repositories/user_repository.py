46a49f106af631c31016cdc245147fa037e89fc1
Kamalesh <kamalcbe86@gmail.com>
2025-01-02 06:03:01 +0000

    user repository refactored

--- src/app/repositories/user_repository.py
+++ src/app/repositories/user_repository.py
@@ -1,15 +1,18 @@
 from uuid import UUID
+
 from sqlalchemy.orm import Session
+
 from src.app.models.data_models import User
 from src.app.repositories.base_repository import BaseRepository
+
 
 class UserRepository(BaseRepository[User]):
     def __init__(self, session: Session):
         self.session = session
-        
+
     def get(self, id: UUID):
         return self.session.query(User).filter(User.user_id == id).first()
-    
+
     def get_all(self):
         return self.session.query(User).all()
 
