f2aea3f0cd744fd7b6aeb3d64fe94608948ee968
Kamalesh <kamalcbe86@gmail.com>
2025-01-06 13:38:27 +0000

    integrated mcq routes in api

--- src/app/routes/api.py
+++ src/app/routes/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.routes.v1 import auth_router
+from app.routes.v1 import auth_router, mcq_routes
 
 router = APIRouter(prefix="/api/v1")
 
 router.include_router(auth_router.router)
+router.include_router(mcq_routes.router)
