7f9628a267106b47c0f0c0bc10cb7a0a60a8b067
Kamalesh <kamalcbe86@gmail.com>
2025-01-06 18:30:16 +0000

    init for all child of unitOfWork

--- src/app/services/__init__.py
+++ src/app/services/__init__.py
@@ -1,10 +1,6 @@
-from .mcq_services import (
-    add_mcq,
-    get_all_mcqs,
-    get_mcqs_by_type,
-    get_user_history,
-    submit_answers,
-)
-from .submission_services import add, get, get_all
-from .unit_of_work import UserUnitOfWork
-from .user_services import add, get, get_all
+from .unit_of_work import (
+    UserUnitOfWork, 
+    McqUnitOfWork, 
+    HistoryUnitOfWork, 
+    SubmissionUnitOfWork,
+    )
