b5968eb160463e38fcc4429a8e6492eb66c52a9c
Kamalesh <kamalcbe86@gmail.com>
2025-01-07 11:51:12 +0000

    service to add bulk insert mcq

--- src/app/services/mcq_services.py
+++ src/app/services/mcq_services.py
@@ -1,4 +1,5 @@
-from fastapi.exceptions import HTTPException
+import pandas as pd
+from fastapi import HTTPException, UploadFile
 
 from app.models.data_models import MCQ
 from app.schemas.mcq_schemas import MCQCreate, MCQCreateOutput, UserOutput
@@ -51,3 +52,64 @@ def add_mcq(
         unit_of_work.session.flush()
         unit_of_work.session.refresh(mcq)
         return MCQCreateOutput(**mcq.__dict__)
+
+
+def bulk_add_mcqs(
+    unit_of_work: BaseUnitOfWork, file: UploadFile, current_user: UserOutput
+) -> int:
+    """
+    Bulk adds MCQs from an uploaded file to the database.
+
+    Args:
+        unit_of_work (BaseUnitOfWork): The unit of work object that manages database transactions and repositories.
+        file (UploadFile): Uploaded Excel file containing MCQ data.
+        current_user (UserOutput): Current authenticated user.
+
+    Returns:
+        int: Count of MCQs successfully added.
+
+    Raises:
+        HTTPException: If the user is not an admin.
+        HTTPException: The file format is invalid.
+    """
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=403, detail="Access denied. Admin role required."
+        )
+
+    if not file.filename.endswith((".xlsx", ".xls")):
+        raise HTTPException(
+            status_code=400, detail="Invalid file format. Upload an Excel file."
+        )
+
+    try:
+        df = pd.read_excel(file.file)
+        added_count = 0
+        with unit_of_work:
+            existing_types = fetch_mcq_types(unit_of_work=unit_of_work)
+            print("Exists: ---> ", existing_types)
+
+            for _, row in df.iterrows():
+                mcq_data = {
+                    "type": row.get("type"),
+                    "question": row.get("question"),
+                    "options": row.get("options"),
+                    "correct_answer": row.get("correct_answer"),
+                    "created_by": current_user.user_id,
+                }
+
+                if mcq_data["type"] not in existing_types:
+                    raise HTTPException(
+                        status_code=400, detail=f"Invalid MCQ type: {mcq_data['type']}"
+                    )
+
+                mcq = MCQ(**mcq_data)
+                unit_of_work.session.add(mcq)
+                added_count += 1
+
+            unit_of_work.session.commit()
+
+        return added_count
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing file: {str(e)}")
