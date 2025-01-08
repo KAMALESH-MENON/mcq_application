be3491513f3125aa23040b4edfb84763fed7c210
Kamalesh <kamalcbe86@gmail.com>
2025-01-08 13:02:23 +0000

    refactored code to use pydantic schema for pagination

--- src/app/services/mcq_services.py
+++ src/app/services/mcq_services.py
@@ -4,8 +4,14 @@ import pandas as pd
 from fastapi import HTTPException, UploadFile
 
 from app.models.data_models import MCQ
-from app.schemas.mcq_schemas import MCQCreate, MCQCreateOutput, UserOutput
+from app.schemas.mcq_schemas import (
+    MCQCreate,
+    MCQCreateOutput,
+    PaginatedResponse,
+    UserOutput,
+)
 from app.services.unit_of_work import BaseUnitOfWork
+from app.utils.model_to_dict import model_to_dict, pydantic_to_dict
 
 
 def fetch_mcq_types(unit_of_work: BaseUnitOfWork) -> list[str]:
@@ -97,10 +103,8 @@ def bulk_add_mcqs(
                     "created_by": current_user.user_id,
                 }
                 mcq = MCQ(**mcq_data)
-                unit_of_work.session.add(mcq)
+                unit_of_work.mcq.add(mcq)
                 added_count += 1
-
-            unit_of_work.session.commit()
 
         return added_count
 
@@ -120,12 +124,17 @@ def get_all(
     with unit_of_work:
         mcqs = unit_of_work.mcq.get_all(type_=type)
 
-        if not mcqs:
+        if mcqs is None:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        mcqs_list_object = [(MCQCreate(**model_to_dict(mcq))) for mcq in mcqs]
+
+        if not mcqs_list_object:
             raise HTTPException(
                 status_code=404, detail="No MCQs found for the given type"
             )
 
-        total_count = len(mcqs)
+        total_count = len(mcqs_list_object)
         total_pages = (total_count // page_size) + (
             1 if total_count % page_size > 0 else 0
         )
@@ -133,15 +142,15 @@ def get_all(
         start = (page - 1) * page_size
         end = start + page_size
 
-        random.shuffle(mcqs)
-        paginated_mcqs = mcqs[start:end]
+        random.shuffle(mcqs_list_object)
+        paginated_mcqs_list_object = mcqs_list_object[start:end]
 
-        response = {
-            "currentPage": page,
-            "totalPage": total_pages,
-            "nextPage": page + 1 if page < total_pages else None,
-            "totalCount": total_count,
-            "data": paginated_mcqs,
-        }
+        next_page = page + 1 if page < total_pages else None
 
-        return response
+        return PaginatedResponse(
+            currentPage=page,
+            totalPage=total_pages,
+            nextPage=next_page,
+            totalCount=total_count,
+            data=paginated_mcqs_list_object,
+        )
