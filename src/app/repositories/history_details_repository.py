93ab9ea701fab151f95fcdb1c9a20b123d8ca78c
Kamalesh <kamalcbe86@gmail.com>
2025-01-21 10:23:15 +0000

    added history details repository

---
+++ src/app/repositories/history_details_repository.py
@@ -0,0 +1,59 @@
+from typing import List
+
+from sqlalchemy.orm import Session
+
+from app.models.data_models import UserHistoryDetail
+from app.repositories.base_repository import BaseRepository
+from app.schemas.mcq_schemas import UserHistoryInput
+
+
+class HistoryDetailsRepository(BaseRepository[UserHistoryDetail]):
+    """A repository class for managing `UserHistoryDetail` objects in the database."""
+
+    def __init__(self, session: Session):
+        """
+        Initialize the HistoryRepository with a database session.
+
+        Parameters: session : Session(SQLAlchemy session object)
+        """
+        self.session = session
+
+    def get(self, detail_id) -> UserHistoryDetail:
+        """
+        Retrieve a single History by their UUID.
+
+        Parameters: history_id : UUID
+
+        Returns: UserHistoryDetail
+            The UserHistoryDetail object
+
+        """
+        submission = (
+            self.session.query(UserHistoryDetail)
+            .filter(UserHistoryDetail.history_id == detail_id)
+            .first()
+        )
+        return submission
+
+    def get_all(self, history_id) -> List[UserHistoryDetail]:
+        """
+        Retrieve all History from the database.
+
+        Returns: List[UserHistoryDetail]
+            A list of UserHistoryDetail objects.
+        """
+        query = self.session.query(UserHistoryDetail)
+
+        if history_id:
+            query = query.filter(UserHistoryDetail.history_id == history_id)
+
+        return query.all()
+
+    def add(self, history: UserHistoryInput):
+        """
+        Add a new History to the database.
+
+        Parameters: user : UserHistoryInput
+            The history details for UserHistoryInput.
+        """
+        self.session.add(history)
